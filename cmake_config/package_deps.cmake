05aff1fd49729297b5a0ddae3846db2f5a3e21d3
H.G <gonghuan.dev@gmail.com>
2018-01-09 16:50:22 +0800

    update cmake file

---
+++ cmake_config/package_deps.cmake
@@ -0,0 +1,8 @@
+
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
+
+find_package(glog)
