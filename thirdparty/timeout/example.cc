76a990d69de401d651cf8d4b4dfc1266a9066be4
H.G <gonghuan.dev@gmail.com>
2018-11-27 07:06:45 +0800

    update

--- thirdparty/timeout/example.cc
+++ thirdparty/timeout/example.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "timeout.h"
 #include <limits.h>
+#include <vector>
 
 static timeout_t random_to(timeout_t min, timeout_t max) {
 	if (max <= min)
@@ -10,12 +11,14 @@ static timeout_t random_to(timeout_t min, timeout_t max) {
 	return min + (rand64 % (max-min));
 }
 int main() {
+
     int err = 0;
     srand (time(NULL));
     struct timeouts *tos = timeouts_open(TIMEOUT_uHZ, &err);
     timeouts_update(tos, 1000);
     timeout_t random_timeout_ms[10];
     struct timeout timers[10] = {0};
+
     for (int i = 0; i < 10; i++) {
         timeout_init(&timers[i], TIMEOUT_ABS);
 		    random_timeout_ms[i] = random_to(0, 20000);
@@ -38,5 +41,13 @@ int main() {
     }
     std::cout << "rollback next tick:" << timeouts_timeout(tos) << std::endl;
 
+    {
+      timeout* to = NULL;
+      TIMEOUTS_FOREACH(to, tos, TIMEOUTS_ALL) {
+        std::cout << "foreach expiretime:" << to->expires << std::endl;
+        timeouts_del(tos, to);
+      }
+      timeouts_close(tos);
+    }
     return 0;
 }
