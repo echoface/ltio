83eeec376343cdb99b87c582fed6b56d39c43be0
gonghuan <gonghuan.dev@gmail.com>
2019-09-11 08:52:11 +0800

    use PROJECT_XXX_DIR replace CMAKE_XXX_DIR for better integration as a sub project

--- cmake/include.cmake
+++ cmake/include.cmake
@@ -1,19 +1,19 @@
 # top level include dir
 
 include_directories(
-  ${CMAKE_SOURCE_DIR}
-  ${CMAKE_SOURCE_DIR}/base
+  ${PROJECT_SOURCE_DIR}
+  ${PROJECT_SOURCE_DIR}/base
 
   #net
-  ${CMAKE_SOURCE_DIR}/net_io
+  ${PROJECT_SOURCE_DIR}/net_io
 
-  ${CMAKE_SOURCE_DIR}/thirdparty
-  ${CMAKE_BINARY_DIR}/thirdparty
+  ${PROJECT_SOURCE_DIR}/thirdparty
+  ${PROJECT_BINARY_DIR}/thirdparty
 
-  ${CMAKE_SOURCE_DIR}/components
+  ${PROJECT_SOURCE_DIR}/components
 
   #coro
-  ${CMAKE_BINARY_DIR}/thirdparty/libcoro
+  ${PROJECT_BINARY_DIR}/thirdparty/libcoro
 
   ${GLOG_INCLUDE_DIR}
 
