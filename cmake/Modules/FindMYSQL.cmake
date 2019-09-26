d9c0b3e44a1f56a6c686d8bda4d512de1b9d8aa6
H.G <gonghuan.dev@gmail.com>
2019-09-26 09:09:12 +0800

    mysql

--- cmake/Modules/FindMYSQL.cmake
+++ cmake/Modules/FindMYSQL.cmake
@@ -2,14 +2,9 @@
 
 # Find the native MySQL includes and library
 #
-#  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
-#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
 #  MYSQL_FOUND       - True if MySQL found.
-
-if(MYSQL_INCLUDE_DIR OR MYSQL_)
-  # Already in cache, be silent
-  SET(MYSQL_FIND_QUIETLY TRUE)
-endif()
+#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
+#  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
 
 if(NOT WIN32)
   find_program(MYSQL_CONFIG_EXECUTABLE mysql_config
@@ -18,6 +13,8 @@ if(NOT WIN32)
     $ENV{MYSQL_DIR}/bin
   )
 endif()
+
+set(MYSQL_NAMES mariadbclient mysqlclient mysqlclient_r)
 
 if(MYSQL_CONFIG_EXECUTABLE)
   execute_process(COMMAND ${MYSQL_CONFIG_EXECUTABLE} --cflags OUTPUT_VARIABLE MYSQL_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -30,62 +27,51 @@ else()
   find_path(MYSQL_INCLUDE_DIR mysql.h
     PATHS
     /usr/include
-    /usr/local/mysql/include
-    /usr/local/include/mysql
     /usr/local/include
     /usr/include/mysql
     /usr/include/mariadb
     /usr/mysql/include
     $ENV{MYSQL_DIR}/include
-    PATH_SUFFIXES
-    lib
-    lib64
-    mysql
-    mariadb
+    /usr/local/mysql/include
+    /usr/local/include/mysql
+    PATH_SUFFIXES include 
   )
-  set(MYSQL_NAMES mariadbclient mysqlclient mysqlclient_r)
-  find_library(MYSQL_LIBRARY NAMES ${MYSQL_NAMES}
+  find_library(MYSQL_LIBRARIES NAMES ${MYSQL_NAMES}
     PATHS
     /usr/lib
     /usr/local/lib
     /usr/local/mysql/lib
     $ENV{MYSQL_DIR}/lib
     $ENV{MYSQL_DIR}/lib/opt
-    PATH_SUFFIXES
-    lib
-    lib64
-    mysql
-    mariadb
+    PATH_SUFFIXES lib lib64 mysql mariadb
   )
-  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
 endif()
 
 if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
-  set(MYSQL_FOUND TRUE)
   if(WIN32)
+    set(MYSQL_LIBRARY ${MYSQL_LIBRARIES})
+    set(MYSQL_INCLUDE_DIR ${MYSQL_INCLUDE_DIR})
     string(REPLACE mysqlclient libmysql libmysql ${MYSQL_LIBRARY})
-    set(MYSQL_LIBRARIES ${libmysql} ${MYSQL_LIBRARIES})
   endif()
 else()
-  set(MYSQL_FOUND FALSE)
-  set(MYSQL_LIBRARIES )
+  set(MYSQL_LIBRARY )
+  set(MYSQL_INCLUDE_DIR )
 endif()
 
+find_package_handle_standard_args(MYSQL DEFAULT_MSG MYSQL_INCLUDE_DIR MYSQL_LIBRARIES)
 if(MYSQL_FOUND)
   if(NOT MYSQL_FIND_QUIETLY)
-    message(STATUS "Found MySQL libraries: ${MYSQL_LIBRARIES}")
-    message(STATUS "Found MySQL includes: ${MYSQL_INCLUDE_DIR}")
+    message(STATUS "Found MySQL: (${MYSQL_INCLUDE_DIR} ${MYSQL_LIBRARIES})")
   endif()
 else()
+  message(STATUS "package MySQL named ${MYSQL_NAMES} NOT FOUND")
   if(MYSQL_FIND_REQUIRED)
-    message(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
     message(FATAL_ERROR "Could NOT find MySQL library")
   endif()
 endif()
 
 mark_as_advanced(
-  MYSQL_CONFIG_EXECUTABLE
   MYSQL_LIBRARY
   MYSQL_INCLUDE_DIR
+  MYSQL_CONFIG_EXECUTABLE
 )
-
